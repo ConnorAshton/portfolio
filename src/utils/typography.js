6e4306cef207931a7040266f024ed08bc9c9a506
Connor Ashton <connor.ashton@theladbiblegroup.com>
2018-05-04 09:15:51 +0100

    Inital Commit

---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    "Avenir Next",
+    "Lato",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Avenir Next", "Lato", "Helvetica Neue", "sans-serif"],
+});
+
+export default typography;
