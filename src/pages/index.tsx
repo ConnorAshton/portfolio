899abb91e92d920a61002f8a8f37c89ef9c5c8f9
Connor Ashton <connor.ashton@theladbiblegroup.com>
2018-05-05 23:22:40 +0100

    Header bar now flex. Home page text finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Link from 'gatsby-link'
 
 import { Viewport } from '../components/component-viewport'
 import { MainBodyHolder } from '../components/component-main-body-holder'
+import { HomepageText } from '../components/component-homepage-text'
 
 export default () => (
       <div>
-            <MainBodyHolder> </MainBodyHolder>
+            <MainBodyHolder> 
+                  <HomepageText/>
+            </MainBodyHolder>
       </div>
 )
