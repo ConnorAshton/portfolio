4aa3d548c5b8ecfa2ceb89daa09a1b74f8ef264a
Connor Ashton <connor.ashton@theladbiblegroup.com>
2018-05-05 20:11:20 +0100

    Main building blocks built and header built

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
 
-import Page from '../components/Page'
-import Container from '../components/Container'
+import { Viewport } from '../components/component-viewport'
+import { MainBodyHolder } from '../components/component-main-body-holder'
 
 export default () => (
-  <Page>
-    <Container>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/" href="/page-2/">Go to page 2</Link>
-    </Container>
-  </Page>
+      <div>
+            <MainBodyHolder> </MainBodyHolder>
+      </div>
 )
